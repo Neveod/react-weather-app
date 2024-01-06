58778ebab9329f3b803a1ec220870c3482f978f4
Neveod <neve@ravenbay.com>
2024-01-06 12:01:33 -0700

    added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This prroject was coded by Neve O'Dwyer and is{" "}
+        <a href="https://github.com/Neveod/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
