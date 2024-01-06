eb393b4fff25745547452f4c4ed1c72748cb0998
Neveod <neve@ravenbay.com>
2024-01-06 13:14:08 -0700

    added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -11,14 +11,11 @@ export default function Weather() {
               type="Search"
               placeholder="Enter a city..."
               className="form-control"
-            ></input>
+              autoFocus="on"
+            />
           </div>
           <div className="col-3">
-            <input
-              type="Submit"
-              value="Search"
-              className="btn btn-primary"
-            ></input>
+            <input type="Submit" value="Search" className="btn btn-primary" />
           </div>
         </div>
       </form>
@@ -33,7 +30,8 @@ export default function Weather() {
             src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
             alt="Cloudy"
           />{" "}
-          6°C
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
         </div>
         <div className="col-6">
           <ul>
